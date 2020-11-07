d36456c596c8f8cea4778fa13a2bf70c737e0a72
SimonImanigirimpuhwe <softimag08@gmail.com>
2020-11-07 08:13:10 +0200

    ft(todo): activate check button

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,11 +8,37 @@ const Todo = ({ todo,todoValue, setTodoValue }) => {
         setTodoValue(todoValue.filter((el) => el.id !== todo.id))
     };
 
+    // handle completed todo
+    const handleCompleted = () => {
+        setTodoValue(todoValue.map(el => {
+            if (el.id === todo.id) {
+                return {
+                    ...el,
+                    completed: !el.completed
+                };
+            }
+            return el;
+        }));
+    }; 
+
     return ( 
         <div className="todo">         
-            <li>{todo.liText}</li>
-            <button className="check"><i className="fa fa-check"></i></button>
-            <button onClick={handleDelete} className="trash"><i className="fa fa-trash"></i></button>
+            <li 
+            className={(todo.completed === true) ? 'checked' : ''}>
+                {todo.liText}
+            </li>
+            
+            <button 
+            onClick={handleCompleted} 
+            className="check">
+                <i className="fa fa-check"></i>
+            </button>
+
+            <button 
+            onClick={handleDelete} 
+            className="trash">
+                <i className="fa fa-trash"></i>
+            </button>
         </div>
      );
 }
