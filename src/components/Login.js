c83df3cb8ec866afb56aac85fe04df76f85025f2
SimonImanigirimpuhwe <softimag08@gmail.com>
2020-11-13 18:34:39 +0200

    feat(card): add auth card layout

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,7 @@
 import React, {useState } from 'react';
 import Button from '@material-ui/core/Button';
 import Snackbar from '@material-ui/core/Snackbar';
+import Card from '@material-ui/core/Card';
 import MuiAlert from '@material-ui/lab/Alert';
 import { makeStyles } from '@material-ui/core/styles';
 import firebase from '../config/firebase.config';
@@ -17,6 +18,27 @@ root: {
     marginTop: theme.spacing(1),
     },
 },
+pos: {
+  marginTop: '-170vh'
+},
+cardRoot: {
+  minWidth: 400,
+  minHeight: 300,
+  display: 'flex',
+  flexDirection:'row',
+  justifyContent: 'center',
+  backgroundColor: 'whitesmoke',
+  boxShadow: '7px 5px 2px #3f51b5',
+  borderRadius: '15px'
+},
+logo: {
+  width: 100,
+  height: 50,
+  alignSelf: 'center',
+},
+btn: {
+  alignSelf: 'center'
+}
 }));
 
 const Login = ({image, setImage, name, setName, handleRouteChange, setUserUid, userUid}) => {
@@ -36,7 +58,7 @@ const Login = ({image, setImage, name, setName, handleRouteChange, setUserUid, u
     const provider = new firebase.auth.GoogleAuthProvider();
 
     const googleSigninFunc = () => {
-  firebase.auth().signInWithPopup(provider).then((result) => {
+    firebase.auth().signInWithPopup(provider).then((result) => {
     const token = result.credential.accessToken;
     const user = result.user;
 
@@ -60,23 +82,26 @@ const Login = ({image, setImage, name, setName, handleRouteChange, setUserUid, u
     return ( 
         <div>
             <div className={classes.root}>
-                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+                <Snackbar open={open} autoHideDuration={6000} onClose={handleClose} >
                     {
                     (message) ? (
-                    <Alert onClose={handleClose} severity="success">
+                    <Alert onClose={handleClose} severity="success" className={classes.pos}>
                         {message}
                     </Alert>
                         ) : (
-                    <Alert onClose={handleClose} severity="error">
+                    <Alert onClose={handleClose} severity="error" className={classes.pos}>
                             {errMessage}
                     </Alert> 
                     )
                     }            
                 </Snackbar>
             </div>
-            <Button  onClick={googleSigninFunc} variant="contained" color="primary">
+            <Card className={classes.cardRoot}>
+            <img src="https://blog.hubspot.com/hubfs/image8-2.jpg" alt="google logo" className={classes.logo}/>
+            <Button  onClick={googleSigninFunc} variant="contained" color="primary" className={classes.btn}>
                 Login with Google
             </Button>
+            </Card>
         </div>
         
      );
